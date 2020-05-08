30c12a2f7a3aba6d24c1e66a7928b43e371c0f11
hasnoone <630222614@qq.com>
2020-05-08 18:50:20 +0800

    测试

--- src/test/java/com/study/dailylearning/DailyLearningApplicationTests.java
+++ src/test/java/com/study/dailylearning/DailyLearningApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(locations = "classpath:ApplicationContext.xml")
 class DailyLearningApplicationTests {
 
 
